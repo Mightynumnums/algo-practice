f4585d615f32600d108cfc85e5496866a27b4647
Mightynumnums <deadcookies@gmail.com>
2020-03-27 13:26:22 -0400

    Adds twoSum problem with tests

---
+++ src/twoSum.js
@@ -0,0 +1,28 @@
+/*
+  Write a function, given an array and a target sum, that returns an array of arrays, that add up to that target sum. No duplicates.
+*/
+const twoSum = (array, sumNum) => {
+  if (!array.length) {
+    throw new Error('Array is empty.')
+  }
+	//get rid of dupes
+  array = new Set(array)
+	// const arr = Array.from(array)
+	const arr = [...array] //or spread that shiz
+  const sums = [];
+  const hashTable = {};
+
+  for (let i = 0; i < arr.length; i++) {
+		let numInArr = arr[i]
+    const sumMinusElement = sumNum - numInArr
+    if (hashTable[sumMinusElement.toString()] !== undefined) {
+      sums.push([numInArr, sumMinusElement])
+    }
+    hashTable[numInArr.toString()] = numInArr;
+  }
+  return sums;
+}
+
+module.exports = {
+  twoSum
+}
---
+++ src/twoSum.spec.js
@@ -0,0 +1,41 @@
+const expect = require('chai').expect
+const {
+  twoSum
+} = require('./twoSum.js')
+
+describe('twoSum', () => {
+
+  describe('twoSum returns a an array of arrays that add up to the sum', () => {
+    it("It's a function", () => {
+      const array = [3, 5, 2, -4, 8, 11, 11, 0, 7]
+      const sumNum = 7
+      const result = twoSum(array, sumNum)
+      expect(typeof twoSum).to.eql('function')
+    })
+    it('Takes in an array as the first argument', () => {
+      const array = [3, 5, 2, -4, 8, 11, 11, 0, 7]
+      expect(Array.isArray(array)).to.eql(true)
+    })
+    it('Can take an empty array as the first argument', () => {
+      const array = []
+      expect(Array.isArray(array)).to.eql(true)
+    })
+    it('If array is empty, throws an error', () => {
+      const array = []
+      const sumNum = 77
+      expect(() => twoSum(array, sumNum)).to.throw()
+    })
+    it('Returns an array', () => {
+      const array = [3, 5, 15, 66, 2, -4, 8, 11, 11, 0, 7]
+      const sumNum = 22
+      const result = twoSum(array, sumNum)
+      expect(Array.isArray(result)).to.eql(true)
+    })
+    it('Returns a correct array', () => {
+      const array = [3, 5, 2, -4, 8, 11, 11, 0, 7]
+      const sumNum = 7
+      const result = twoSum(array, sumNum)
+      expect(result).to.eql([ [ 2, 5 ], [ 11, -4 ], [ 7, 0 ] ])
+    })
+  })
+})
