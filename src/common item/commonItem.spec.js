31e31d499a9258e52f7e9745c8635d59b29bd0c8
Mightynumnums <deadcookies@gmail.com>
2020-06-02 18:53:37 -0400

    Adds all of the tests for common item problem

--- src/common item/commonItem.spec.js
+++ src/common item/commonItem.spec.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect
-const { commonItem } = require('./commonItem')
+const { containsCommon} = require('./commonItem')
 
 describe('Common Item in an array problem', () => {
   describe('commonIem', () => {
@@ -18,6 +18,24 @@ describe('Common Item in an array problem', () => {
       let arg1 = [1,2,3]
       let arg2 = 'apple'
       expect(Array.isArray(arg1) && Array.isArray(arg2)).to.eql(false)
+    })
+    it('tests and returns true if there is a common item', () => {
+      let arg1 = ['a', 'b', 'c', 'x']
+      let arg2 = ['d', 'x', 'g']
+      let result  = containsCommon(arg1, arg2)
+      expect(result).to.eql(true)
+    })
+    it('tests and returns false if there is NO common item', () => {
+      let arg1 = ['a', 'b', 'c', 'x']
+      let arg2 = ['d', 'u', 'g']
+      let result  = containsCommon(arg1, arg2)
+      expect(result).to.eql(false)
+    })
+    it('tests and returns false if there is NO common item, and if one of the arrays is empty', () => {
+      let arg1 = []
+      let arg2 = ['d', 'u', 'g']
+      let result  = containsCommon(arg1, arg2)
+      expect(result).to.eql(false)
     })
   })
 })
