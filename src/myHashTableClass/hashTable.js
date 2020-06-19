a9098d93ebf5aca257fc0cf712f3f7b94f5b71f9
Mightynumnums <deadcookies@gmail.com>
2020-06-19 17:09:52 -0400

    Alters the keys() and values methods to handle collissions and alters the tests

--- src/myHashTableClass/hashTable.js
+++ src/myHashTableClass/hashTable.js
@@ -42,26 +42,49 @@ class HashTable {
 			}
 		}
 	}
+keys() {
+    if (!this.data.length) {
+      return undefined
+    }
+    let result = []
+    // loop through all the elements
+    for (let i = 0; i < this.data.length; i++) {
+        // if it's not an empty memory cell
+        if (this.data[i] && this.data[i].length) {
+          // but also loop through all the potential collisions
+          if (this.data.length > 1) {
+            for (let j = 0; j < this.data[i].length; j++) {
+              result.push(this.data[i][j][0])
+            }
+          } else {
+            result.push(this.data[i][0])
+          }
+        }
+    }
+    return result;
+  }
 
-	keys() {
-		let keysArray = []
-		for(let i = 0; i < this.data.length; i++) {
-			if (this.data[i]) {
-				keysArray.push(this.data[i][0][0])
-			}
-		}
-		return keysArray
-	}
-
-	values() {
-		let valuesArray = []
-		for(let i = 0; i < this.data.length; i++) {
-			if (this.data[i]) {
-				valuesArray.push(this.data[i][0][1])
-			}
-		}
-		return valuesArray
-	}
+values() {
+    if (!this.data.length) {
+      return undefined
+    }
+    let valuesArray = []
+    // loop through all the elements
+    for (let i = 0; i < this.data.length; i++) {
+        // if it's not an empty memory cell
+        if (this.data[i] && this.data[i].length) {
+          // but also loop through all the potential collisions
+          if (this.data.length > 1) {
+            for (let j = 0; j < this.data[i].length; j++) {
+              valuesArray.push(this.data[i][j][1])
+            }
+          } else {
+            valuesArray.push(this.data[i][0])
+          }
+        }
+    }
+    return valuesArray;
+  }
 }
 
 module.exports = {
